3d1cba2cdfff7a2e5c4b5d62ec870aa7e46239e2
gahan9 <gahansaraiya@gmail.com>
2018-04-09 00:21:34 +0530

    set sqlite for both server and local

--- core_settings/__init__.py
+++ core_settings/__init__.py
@@ -2,6 +2,7 @@ import os
 host_name = os.popen('hostname -I').read().strip()
 
 if host_name.startswith("10.0"):
-    from .server_settings import *
+    from .local_settings import *
+    # from .server_settings import *
 else:
     from .local_settings import *
