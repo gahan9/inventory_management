e246a36af44fd0f4f7037a858629baf74bbcd1fb
gahan9 <gahansaraiya@gmail.com>
2018-06-07 18:14:26 +0530

    settings updated

--- core_settings/__init__.py
+++ core_settings/__init__.py
@@ -4,5 +4,16 @@ host_name = os.popen('hostname -I').read().strip() if os.sys.platform != "win32"
 if host_name.startswith("10.0"):
     # from .local_settings import *
     from .server_settings import *
+elif host_name.startswith("46.101.17.77"):
+    from .settings import *
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {
+                'read_default_file': os.path.join(BASE_DIR, 'mysql_digital_ocean.conf'),
+            },
+        }
+    }
 else:
     from .local_settings import *
