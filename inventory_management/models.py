b285b3e7422c2a1a916dbf468f6a725abf8aff2a
Gahan Saraiya <gahansaraiya@gmail.com>
2022-03-13 23:03:11 +0530

    Upgraded project for django-4

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from main.models import *
 from core_settings.settings import PRODUCT_TYPE, PRODUCT_MAKER
 
